9280f84d5945e23c9a860e9182644e542426e494
aesperer <piyrw9754@gmail.com>
2023-04-05 08:13:13 +0900

    Hello Swift UI

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,20 +7,40 @@
 
 import SwiftUI
 
-struct ContentView: View {
+    struct ContentView: View {
+        
+    @State var touchedCount = 0
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello Swift")
         }
         .padding()
+        
+        NavigationView{
+            Form{
+                Text("버튼을 클릭한 횟수 \(touchedCount)")
+                Button("this is button"){
+                    self.touchedCount += 1
+                }
+            }.navigationBarTitle("this is title")
+        }
+        
+        Form{
+            Text("가짜 Todomate")
+            Group{
+                Text("asdfasdfasdfasdf")
+                Text("asdfasdfasdfasdf")
+                Text("Hello Swift UI")
+            }
+        }
     }
-}
+    }
 
-struct ContentView_Previews: PreviewProvider {
+    struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
+    }
