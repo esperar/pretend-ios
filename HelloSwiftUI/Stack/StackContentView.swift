c5c1873cb0133e91186c2d3d8c857d4afaf3cc95
aesperer <piyrw9754@gmail.com>
2023-04-05 10:24:54 +0900

    sign, stack

---
+++ HelloSwiftUI/Stack/StackContentView.swift
@@ -0,0 +1,20 @@
+//
+//  StackContentView.swift
+//  HelloSwiftUI
+//
+//  Created by 김희망 on 2023/04/05.
+//
+
+import SwiftUI
+
+struct StackContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StackContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        StackContentView()
+    }
+}
