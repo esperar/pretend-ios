74f4dc9319b03a9563d0eeb6d641be1789c225f0
aesperer <piyrw9754@gmail.com>
2023-04-11 08:21:23 +0900

    animation duratino

---
+++ HelloSwiftUI/Tutorial/Animation/AnimationView3.swift
@@ -0,0 +1,61 @@
+//
+//  AnimationView3.swift
+//  HelloSwiftUI
+//
+//  Created by 김희망 on 2023/04/11.
+//
+
+import SwiftUI
+
+struct AnimationView3: View {
+    
+    @State var animate: Bool = false
+    let timing: Double = 10.0
+    
+    var body: some View {
+        
+        VStack{
+            Button("애니메이션") {
+                animate.toggle()
+            }
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: animate ? 300 : 200, height: 100)
+                .overlay(
+                    Text("animation: default")
+                        .foregroundColor(Color.white)
+                )
+                .animation(Animation.default)
+            
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: animate ? 300 : 200, height: 100)
+                .overlay(
+                    Text("animation: easeOut")
+                        .foregroundColor(Color.white)
+                )
+                .animation(Animation.easeOut(duration: timing))
+            
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: animate ? 300 : 200, height: 100)
+                .overlay(
+                    Text("animation: easeIn")
+                        .foregroundColor(Color.white)
+                )
+                .animation(Animation.easeIn(duration: timing))
+            
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: animate ? 300 : 200, height: 100)
+                .overlay(
+                    Text("animation: linear")
+                        .foregroundColor(Color.white)
+                )
+                .animation(Animation.linear(duration: timing))
+        }
+        
+    }
+}
+
+struct AnimationView3_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimationView3()
+    }
+}
